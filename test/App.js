24c13fa669c479bf7f37b941123e29ea61e9ef28
xudan0812 <xudan0812@gmail.com>
2018-09-11 19:42:22 -0400

    Initial commit.

---
+++ test/App.js
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+import Header from './src/components/header';
+
+export default class App extends Component<> {
+  render() {
+    return (
+      <Header />
+    );
+  }
+}

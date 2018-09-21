11cb276327c959dcea520b4bd2bcf58e0e971164
xudan0812 <xudan0812@gmail.com>
2018-09-20 23:15:35 -0400

    Finished album app.

--- test/App.js
+++ test/App.js
@@ -9,11 +9,15 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
 import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 
 export default class App extends Component<> {
   render() {
     return (
-      <Header />
+      <View style = {{flex: 1}}>
+        <Header headerText={'Album'}/>
+        <AlbumList />
+      </View>
     );
   }
 }
---
+++ test/src/components/AlbumList.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import { ScrollView } from 'react-native';
+import AlbumDetail from './AlbumDetail';
+
+class AlbumList extends Component {
+  state = { albums: []};
+
+  componentWillMount() {
+    fetch('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => response.json())
+      .then(parsedData => this.setState({ albums: parsedData}));
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album =>
+      <AlbumDetail key = {album.title} album = {album} />
+    );
+  }
+
+  render() {
+    console.log(this.state);
+
+    return (
+      <ScrollView>
+        {this.renderAlbums()}
+      </ScrollView>
+    );
+  }
+}
+
+export default AlbumList;
